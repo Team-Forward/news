13769a1482318a216ca27062d32792de7e775b36
Aurélien <dav.aurelien@gmail.com>
2021-02-17 10:53:43 +0100

    Update fixture shareitem
    Signed-off-by: Marco Nassabain <marco.nassabain@hotmail.com>

--- tests/Integration/Fixtures/data/shareitem.php
+++ tests/Integration/Fixtures/data/shareitem.php
@@ -17,6 +17,7 @@ return [
             'items' => [
                 ['title' => 'article 1', 'unread' => true],
                 ['title' => 'article 2', 'unread' => true],
+                ['title' => 'article 2', 'sharedBy' => 'test', 'sharedWith' => 'aurélien']
             ]
         ],
         [
@@ -27,7 +28,24 @@ return [
                 ['title' => 'article 2', 'unread' => true],
                 ['title' => 'article 3', 'unread' => true],
                 ['title' => 'article 4', 'unread' => true],
-                ['title' => 'article 1', 'unread' => true, 'sharedBy' => 'marco', 'sharedWith' => 'test']
+                ['title' => 'article 1', 'unread' => true, 'sharedBy' => 'marco', 'sharedWith' => 'test'],
+                
+            ]
+        ],
+        [
+            'title' => 'marco feed',
+            'userId' => 'aurélien',
+            'items' => [
+                ['title' => 'article 10', 'unread' => true],
+                ['title' => 'article 11', 'unread' => true],
+                ['title' => 'article 10', 'unread' => true, 'sharedBy' => 'aurélien', 'sharedWith' => 'marco'],
+            ]
+        ],
+        [
+            'title' => 'nicolas feed',
+            'userId' => 'nicolas',
+            'items' => [
+                ['title' => 'article 100', 'unread' => true]
             ]
         ]
     ]
