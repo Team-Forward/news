bc16da5ce432827b21f22553c226399a08a6965f
Marco Nassabain <marco.nassabain@hotmail.com>
2021-02-19 16:22:46 +0100

    Updated language translation files: - is - it - ja - ka_GE - km - kn - ko - lb - lt_LT - lv - mk - mn - ms_MY
    Signed-off-by: Marco Nassabain <marco.nassabain@hotmail.com>

--- l10n/ms_MY.js
+++ l10n/ms_MY.js
@@ -19,6 +19,14 @@ OC.L10N.register(
     "Import" : "Import",
     "Export" : "Export",
     "Help" : "Bantuan",
-    "Documentation" : "Dokumentasi"
+    "Documentation" : "Dokumentasi",
+
+    "Contacts" : "Kenalan",
+    "Share on social media" : "Kongsi di media sosial",
+    "Contact name" : "Nama kenalan",
+    "shared from" : "dikongsi dari",
+    "Shared": "Dikongsi",
+    "Shared articles" : "Artikel yang dikongsi",
+    "Users" : "Pengguna"
 },
 "nplurals=1; plural=0;");
