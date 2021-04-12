2f53e0a0d9f149b1863709d18add9529fe8458c6
Marco Nassabain <marco.nassabain@hotmail.com>
2021-04-12 16:14:57 +0200

    ✅ UserLoggedInListener: test handle function
    Co-authored-by: Nicolas Wendling <NicoWend@users.noreply.github.com>
    Signed-off-by: Marco Nassabain <marco.nassabain@hotmail.com>

--- tests/Unit/Listener/UserLoggedInListenerTest.php
+++ tests/Unit/Listener/UserLoggedInListenerTest.php
@@ -15,10 +15,13 @@ use OCA\News\Listener\UserLoggedInListener;
 use OCA\News\Service\FeedServiceV2;
 use OCA\News\Service\FolderServiceV2;
 use OCP\User\Events\UserLoggedInEvent;
+use OCA\News\AppInfo\Application;
 use OCP\EventDispatcher\Event;
+use OCA\News\Db\Folder;
 use OCA\News\Db\Feed;
 use OCP\IConfig;
 use OCP\IL10N;
+use OCP\IUser;
 
 use PHPUnit\Framework\TestCase;
 
@@ -81,10 +84,60 @@ class UserLoggedInListenerTest extends TestCase
         );
     }
 
-    // public function handleTest()
-    // {
+    public function testHandle()
+    {
+        $userId = 'nicolas';
+        $defaultFeeds = json_encode(['url 1', 'url 2']);
+        $folderName = 'Recommended feeds';
+        $folder = new Folder();
+        $folder->setId(1);
+        $existingFeed = new Feed();
 
-    // }
+        $user = $this->getMockBuilder(IUser::class)
+            ->getMock();
+
+        $event = $this->getMockBuilder(UserLoggedInEvent::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $event->expects($this->once())
+            ->method('getUser')
+            ->will($this->returnValue($user));
+
+        $user->expects($this->once())
+            ->method('getUID')
+            ->will($this->returnValue($userId));
+
+        $this->settings->expects($this->once())
+            ->method('getAppValue')
+            ->with(Application::NAME,
+                'defaultFeeds',
+                Application::DEFAULT_SETTINGS['defaultFeeds']
+            )
+            ->will($this->returnValue($defaultFeeds));
+
+        $this->l10n->expects($this->once())
+            ->method('t')
+            ->with($folderName)
+            ->will($this->returnValue($folderName));
+
+        $this->folderService->expects($this->once())
+            ->method('findFromUserByName')
+            ->with($userId, $folderName)
+            ->will($this->returnValue($folder));
+
+        $this->feedService->expects($this->exactly(2))
+            ->method('existsForUser')
+            ->withConsecutive([$userId, 'url 1'], [$userId, 'url 2'])
+            ->willReturnOnConsecutiveCalls(false, true);
+
+        $this->feedService->expects($this->once())
+            ->method('create')
+            ->with($userId, 'url 1', 1);
+
+
+        $this->class->handle($event);
+    }
 
     public function testAddFeed()
     {
