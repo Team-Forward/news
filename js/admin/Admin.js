f6a2b0f36f70226651f93ef952f701534195d14b
Marco Nassabain <marco.nassabain@hotmail.com>
2021-04-12 15:10:52 +0200

    Revert "✨ Adding defaultHashtags into admin parameters"
    This reverts commit 82a93e810935e2ed96f0c353385bb40105e582b4.

--- js/admin/Admin.js
+++ js/admin/Admin.js
@@ -17,7 +17,6 @@
     $(document).ready(function () {
 
         var listFeeds = [];
-        var listHashtags_result = [];
 
         var useCronUpdatesInput =
             $('#news input[name="news-use-cron-updates"]');
@@ -42,12 +41,6 @@
         listRss.each(function(index, objRss) {
             console.log('====', objRss.innerText);
             listFeeds.push(objRss.innerText);
-        });
-
-        var listHashtags = $('#news a[name="news-feed-element-hashtag"]')
-        listHashtags.each(function(index, objRss) {
-            console.log('====', objHash.innerText);
-            listHashtags_result.push(objHash.innerText);
         });
 
         // var listRss = $('#bricolage').val();
@@ -90,8 +83,7 @@
                 useCronUpdates: useCronUpdates,
                 exploreUrl: exploreUrl,
                 updateInterval: parseInt(updateInterval, 10),
-                defaultFeeds: JSON.stringify(listFeeds),
-                defaultHashtags: JSON.stringify(listHashtags_result)
+                defaultFeeds: JSON.stringify(listFeeds)
             };
 
             var url = OC.generateUrl('/apps/news/admin');
@@ -122,13 +114,6 @@
             listFeeds.push(urlFlux);
             submit();
         });
-
-        $( "#addHashtag" ).click(function() {
-            var hashtagVal = $("#hashtag").val();
-            listHashtags_result.push(hashtagVal);
-            submit();
-        });
-
 
         $('#news input[type="text"][name!="urlFlux"]').blur(submit);
         $('#news input[type="checkbox"]').change(submit);
